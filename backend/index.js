ad79ad91248e3acf60e1e0316c7b03ea5aa8d7b3
mayank6386 <150098219+mayank6386@users.noreply.github.com>
2026-04-14 23:21:18 +0530

    Update CORS configuration to allow multiple origins

--- backend/index.js
+++ backend/index.js
@@ -13,9 +13,12 @@ const port=process.env.PORT || 5000
 
 
 app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
-}))
+  origin: [
+    "http://localhost:5173",
+    "https://realtimechatapp-1-iriy.onrender.com"
+  ],
+  credentials: true
+}));
 app.use(express.json())
 app.use(cookieParser())
 app.use("/api/auth",authRouter)
