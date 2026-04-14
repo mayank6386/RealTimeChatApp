b62523585474eccb7c17d8870123f6f2931c2d76
mayank6386 <150098219+mayank6386@users.noreply.github.com>
2026-04-15 00:30:11 +0530

    Update Socket.IO CORS settings
    Simplified CORS configuration for Socket.IO by removing local origin.

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -5,15 +5,11 @@ import { Server } from "socket.io"
 let app = express()
 
 const server=http.createServer(app)
-const io = new Server(server, {
-  cors: {
-    origin: [
-      "http://localhost:5173",
-      "https://realtimechatapp-1-iriy.onrender.com"
-    ],
-    credentials: true
-  }
-});
+const io=new Server(server,{
+    cors:{
+        origin:"https://realtimechatapp-1-iriy.onrender.com"
+    }
+})
  const userSocketMap ={}
  export const getReceiverSocketId=(receiver)=>{
     return userSocketMap[receiver]
