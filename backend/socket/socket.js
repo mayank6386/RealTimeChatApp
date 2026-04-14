011c1b8ca1c8e8dc9ae7e7bef9bb45f5a02b137d
mayank6386 <150098219+mayank6386@users.noreply.github.com>
2026-04-15 00:09:38 +0530

    Update CORS configuration for socket server

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -5,11 +5,15 @@ import { Server } from "socket.io"
 let app = express()
 
 const server=http.createServer(app)
-const io=new Server(server,{
-    cors:{
-        origin:"http://localhost:5173"
-    }
-})
+const io = new Server(server, {
+  cors: {
+    origin: [
+      "http://localhost:5173",
+      "https://realtimechatapp-1-iriy.onrender.com"
+    ],
+    credentials: true
+  }
+});
  const userSocketMap ={}
  export const getReceiverSocketId=(receiver)=>{
     return userSocketMap[receiver]
